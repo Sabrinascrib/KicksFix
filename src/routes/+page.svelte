efa0aac245bd7ea9c9876343fb0fc74d86bb131e
sabrina <sabrinaabrodi20@gmail.com>
2022-12-23 15:11:11 +0100

    Feat: creating hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import homeplush from '$lib/assets/home-plush.jpg';
+</script>
+
+<section class="header flex flex-row place-items-center justify-center mx-10 max-w-[3000px]">
+	<div class="header-info w-1/2 ">
+		<h2 class="text-[3vw] font-extrabold text-sky-200 mb-4">Fluffy, Cute, Sanrio.</h2>
+
+		<p class="text-[1.5vw] text-rose-200 font-semibold mb-6">Get your sanrio plushie now!</p>
+		<a class="text-[1vw] bg-sky-200 py-2 px-10 rounded-full text-white" href="/items">Buy Now</a>
+	</div>
+	<div class="w-1/2">
+		<img class="" src={homeplush} alt="blue plushie" />
+	</div>
+</section>
