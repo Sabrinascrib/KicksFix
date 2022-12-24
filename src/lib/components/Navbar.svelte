6931e66de5f0dc4318426374db1cc1e544ddf13a
sabrina <sabrinaabrodi20@gmail.com>
2022-12-24 20:03:06 +0100

    feat: navbar structure

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<script>
+	import stars from '$lib/assets/stars.png';
+</script>
+
+<div
+	class="w-full h-20 flex flex-row justify-between place-items-center mx-auto  px-10 max-w-[1500px]"
+>
+	<div class="link ">
+		<a class="text-rose-200 font-bold text-lg" href="/">Home</a>
+		<a class="text-sky-200 font-bold ml-8 text-lg" href="/items">Shop</a>
+	</div>
+	<h3 class="text-xl uppercase font-bold text-rose-200">Shop Rio</h3>
+	<a class="text-3xl text-sky-200" href="/cart"
+		><svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			preserveAspectRatio="xMidYMid meet"
+			viewBox="0 0 256 256"
+			><path
+				fill="currentColor"
+				d="M100 216a20 20 0 1 1-20-20a20.1 20.1 0 0 1 20 20Zm84-20a20 20 0 1 0 20 20a20.1 20.1 0 0 0-20-20Zm49.3-120.7l-24.2 84.4a28.1 28.1 0 0 1-26.9 20.3H81.8a28.1 28.1 0 0 1-26.9-20.3L30.8 75.5c0-.1-.1-.3-.1-.4L21.8 44H12a12 12 0 0 1 0-24h12.8a20.3 20.3 0 0 1 19.3 14.5L51.3 60h170.4a12.1 12.1 0 0 1 11.6 15.3ZM205.8 84H58.2l19.7 69.1a4.1 4.1 0 0 0 3.9 2.9h100.4a4.1 4.1 0 0 0 3.9-2.9Z"
+			/></svg
+		></a
+	>
+</div>
+
+<!-- ffa0a5 -->
