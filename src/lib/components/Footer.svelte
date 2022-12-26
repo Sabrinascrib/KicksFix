ad13dc28fb0b74460f27c4f95d1b6ad82c90242a
sabrina <sabrinaabrodi20@gmail.com>
2022-12-26 14:58:51 +0100

    fix: add target blank

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,7 @@
-<div class="bg-[#f6f6f6] w-full text-center">
+<div class="bg-[#f6f6f6] w-full text-center bottom-0">
 	<p class="font-semibold py-10">
 		Build by sneaker lover <a
+			target="_blank"
 			href="https://github.com/Sabrinascrib/KicksFix"
 			class="text-[#fa0f0f] duration-100 hover:text-[#b01919]">Sabrina Abrodi</a
 		>
