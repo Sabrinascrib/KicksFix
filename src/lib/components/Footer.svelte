b11b02b71415c511d16e9979166451e7d636a8fe
sabrina <sabrinaabrodi20@gmail.com>
2022-12-26 13:54:22 +0100

    feat: footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<div class="bg-[#f6f6f6] w-full text-center">
+	<p class="font-semibold py-10">
+		Build by sneaker lover <a href="" class="text-[#fa0f0f] duration-100 hover:text-[#b01919]"
+			>Sabrina Abrodi</a
+		>
+	</p>
+</div>
