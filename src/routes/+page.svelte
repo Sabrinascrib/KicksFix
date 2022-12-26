a73420dfbabfc685a07fc469c4afc1333e4dc071
sabrina <sabrinaabrodi20@gmail.com>
2022-12-26 14:59:05 +0100

    fix: more styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import balckcanvas from '$lib/assets/black-canvas.jpg';
+	import firered from '$lib/assets/fire-red.png';
+
 	import Footer from '$lib/components/Footer.svelte';
 
 	import Navbar from '$lib/components/Navbar.svelte';
@@ -7,15 +8,20 @@
 
 <Navbar />
 
-<section class="hero my-40 flex flex-row place-items-center justify-center mx-10 max-w-[3000px]">
-	<div class="hero-info  ">
-		<h2 class="text-[50px] font-bold text-black mb-4 max-w-[600px] uppercase">
+<section
+	class="hero mb-[550px] mt-56 flex flex-row place-items-center justify-center mx-10 max-w-[3000px]"
+>
+	<div class="hero-info mr-[-100px] ">
+		<h2 class="text-[50px] font-bold text-black mb-4 max-w-[400px] uppercase">
 			Get yourself a pair of jordan 4's now!
 		</h2>
-		<a class="text-lg bg-[#fa0f0f] py-2 px-10 uppercase text-white" href="/items">Shop</a>
+		<a
+			class="text-lg bg-[#fa0f0f]  py-2 duration-100 px-10 uppercase text-white hover:bg-[#d61212] "
+			href="/items">Shop</a
+		>
 	</div>
 	<div class="">
-		<img class="w-[500px]" src={balckcanvas} alt="blue plushie" />
+		<img class="w-[700px] mb-[-300px] ml-[-20px]" src={firered} alt="blue plushie" />
 	</div>
 </section>
 
@@ -24,7 +30,7 @@
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap');
 	.hero {
-		font-family: 'Poppins', sans-serif;
+		font-family: 'Bebas Neue', cursive;
 	}
 
 	a {
