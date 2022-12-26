86e8c3622e29ca7458edcbc27879ea361d60fe56
sabrina <sabrinaabrodi20@gmail.com>
2022-12-26 13:59:24 +0100

    fix: adding link to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,8 @@
 <div class="bg-[#f6f6f6] w-full text-center">
 	<p class="font-semibold py-10">
-		Build by sneaker lover <a href="" class="text-[#fa0f0f] duration-100 hover:text-[#b01919]"
-			>Sabrina Abrodi</a
+		Build by sneaker lover <a
+			href="https://github.com/Sabrinascrib/KicksFix"
+			class="text-[#fa0f0f] duration-100 hover:text-[#b01919]">Sabrina Abrodi</a
 		>
 	</p>
 </div>
