1e00a92865a07fd3eda6b0b6e76e9316b8383ad3
sabrina <sabrinaabrodi20@gmail.com>
2022-12-28 14:59:41 +0100

    feat: importing navbar and footer

--- src/routes/product/[slug]/+page.svelte
+++ src/routes/product/[slug]/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from './../../../lib/components/Footer.svelte';
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
+
+<Navbar />
 
 <div class="">
 	<img src={data.item.img} alt={data.item.title} />
@@ -10,3 +14,5 @@
 		<p>{data.item.price}</p>
 	</div>
 </div>
+
+<Footer />
