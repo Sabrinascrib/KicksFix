9b282810b258a865194f53c0e57a76a1ed9dacc0
sabrina <sabrinaabrodi20@gmail.com>
2022-12-28 14:56:40 +0100

    feat: displaying data

---
+++ src/routes/product/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<div class="">
+	<img src={data.item.img} alt={data.item.title} />
+	<div class="">
+		<h2>{data.item.title}</h2>
+		<p>{data.item.price}</p>
+	</div>
+</div>
