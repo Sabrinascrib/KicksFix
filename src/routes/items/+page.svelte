6d820d54cf80e7e7f7dede91888ecf3c09351fd4
sabrina <sabrinaabrodi20@gmail.com>
2023-01-04 15:10:17 +0100

    fix: change font size on btn

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -61,7 +61,7 @@
 						</div>
 					</div>
 					<button
-						class="bg-[#fa0f0f] hover:bg-[#ea1717] duration-75 text-white text-lg tracking-wider font-semibold rounded-sm mt-4 h-8 px-2 "
+						class="bg-[#fa0f0f] hover:bg-[#ea1717] duration-75 text-white text-md tracking-wider font-semibold rounded-sm mt-4 h-8 px-2 "
 						on:click={() => addToCart(product)}>Add to cart</button
 					>
 				</div>
