5d683151af76adb0fe9ee70c4ea9e07112012037
sabrina <sabrinaabrodi20@gmail.com>
2023-01-07 20:09:39 +0100

    fix: turning inte ts file

--- src/routes/product/[slug]/+page.ts
+++ src/routes/product/[slug]/+page.ts
@@ -1,12 +1,12 @@
 /** @type {import('./$types').PageLoad} */
 
+import { products } from '$lib/data.js';
+
 export function load({ params }) {
-	const product = products.find((element) => element.id == `${params.slug}`);
+	const product = products.find((element) => element.id === `${params.slug}`);
 
 	console.log(product);
 	return {
 		product
 	};
 }
-
-import { products } from '$lib/data.js';
