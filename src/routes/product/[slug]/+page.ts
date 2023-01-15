b34bfea55bffcd2651b0640afb322f7ee6653f2c
sabrina <sabrinaabrodi20@gmail.com>
2023-01-15 18:46:22 +0100

    remove console log

--- src/routes/product/[slug]/+page.ts
+++ src/routes/product/[slug]/+page.ts
@@ -5,7 +5,6 @@ import { products } from '$lib/data.js';
 export function load({ params }) {
 	const product = products.find((element) => element.id === `${params.slug}`);
 
-	console.log(product);
 	return {
 		product
 	};
